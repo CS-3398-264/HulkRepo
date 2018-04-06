08671ab23cc4e14d91c4fff151d2abdd554f76ec
Nano95 <30358490+Nano95@users.noreply.github.com>
2018-04-05 20:16:59 -0500

    Add files via upload

--- user/user.js
+++ user/user.js
@@ -3,22 +3,9 @@
 var mongoose = require('mongoose');
 var UserSchema = new mongoose.Schema({
 	name: String,
-	email: String,
-	password: String
+	longitude: Number,
+	latitude: Number,
+	rating: Number
 });
 mongoose.model('User', UserSchema);
 module.exports = mongoose.model('User');
-
-var NuberDriverSchema = new mongoose.Schema({
-	name: String,
-	xCoordinate: int,
-	yCoordinate: int,
-	rating: Int16Array
-});
-
-var NuberCustomerSchema = new mongoose.Schema({
-	name: String,
-	xCoordinate: float,
-	yCoordinate: float,
-	active: Boolean
-});
--- user/userController.js
+++ user/userController.js
@@ -18,22 +18,6 @@ router.use(bodyParser.json());
 var User = require('./User');
 module.exports = router;
 
-// CREATES A NEW USER
-
-/**first param: route which will be linked to a function. 
-*  Second param: function. 1st param request to server. 2nd response from server
-*/
-router.post('/', function (req, res) {
-	User.create({
-		name: req.body.name,
-		email: req.body.email,
-		password: req.body.password
-	},
-        function (err, user) {
-        	if (err) return res.status(500).send("There was a problem adding the information to the database.");
-        	res.status(200).send(user);
-        });
-});
 // RETURNS ALL THE USERS IN THE DATABASE
 router.get('/', function (req, res) {
 	User.find({}, function (err, users) {
@@ -42,27 +26,3 @@ router.get('/', function (req, res) {
 	});
 });
 
-// GETS A SINGLE USER FROM THE DATABASE
-router.get('/:id', function (req, res) {
-	User.findById(req.params.id, function (err, user) {
-		if (err) return res.status(500).send("There was a problem finding the user.");
-		if (!user) return res.status(404).send("No user found.");
-		res.status(200).send(user);
-	});
-});
-
-// DELETES A USER FROM THE DATABASE
-router.delete('/:id', function (req, res) {
-	User.findByIdAndRemove(req.params.id, function (err, user) {
-		if (err) return res.status(500).send("There was a problem deleting the user.");
-		res.status(200).send("User: " + user.name + " was deleted.");
-	});
-});
-
-// UPDATES A SINGLE USER IN THE DATABASE
-router.put('/:id', function (req, res) {
-	User.findByIdAndUpdate(req.params.id, req.body, { new: true }, function (err, user) {
-		if (err) return res.status(500).send("There was a problem updating the user.");
-		res.status(200).send(user);
-	});
-});
