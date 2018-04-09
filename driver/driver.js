414cbb9ca4bf3038971e9ee10672c37c9fc11846
Nano95 <30358490+Nano95@users.noreply.github.com>
2018-04-08 22:04:44 -0500

    Added latitude, and longitude attr's.

--- driver/driver.js
+++ driver/driver.js
@@ -1,11 +1,9 @@
-// User.js -- You’re creating a schema which 
-// will give every user in the database a specific look.
 var mongoose = require('mongoose');
 var DriverSchema = new mongoose.Schema({
 	name: String,
-	available: Boolean
-
+	available: Boolean,
+	latitude: Number,
+	longitude: Number
 });
 mongoose.model('Driver', DriverSchema);
 module.exports = mongoose.model('Driver');
-
