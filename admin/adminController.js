a1927697ede891d69da05267ad0cd3dbdddc38a3
Nano95 <30358490+Nano95@users.noreply.github.com>
2018-04-08 22:03:30 -0500

    Cleaning up

--- admin/adminController.js
+++ admin/adminController.js
@@ -1,7 +1,7 @@
 // adminController.js
 // This is the bare layout of the admin controller
 
-// Express router used tocreate a subset of routes 
+// Express router used to create a subset of routes 
 // which can be modular and independent from the whole app
 var express = require('express');
 var router = express.Router();
@@ -12,13 +12,10 @@ var bodyParser = require('body-parser');
 router.use(bodyParser.urlencoded({ extended: true }));
 router.use(bodyParser.json());
 
-// Whenever you create a model like you did in user.js, it automagically 
-// receives all the necessary methods for interacting with 
-// a database, including create, read, update and delete actions
 var Admin = require('./Admin');
 module.exports = router;
 
-// CREATES A NEW USER
+// CREATES A NEW ADMIN
 /**first param: route which will be linked to a function. 
 *  Second param: function. 1st param request to server. 2nd response from server
 */
@@ -26,72 +23,40 @@ router.post('/', function (req, res) {
 	Admin.create({
 		name: req.body.name
 	},
-        function (err, user) {
+        function (err, admin) {
         	if (err) return res.status(500).send("There was a problem adding the information to the database.");
-        	res.status(200).send(user);
+        	res.status(200).send(admin);
         });
 });
-// RETURNS ALL THE USERS IN THE DATABASE
+// RETURNS ALL THE ADMINS IN THE DATABASE
 router.get('/', function (req, res) {
-	Admin.find({}, function (err, users) {
-		if (err) return res.status(500).send("There was a problem finding the users.");
-		res.status(200).send(users);
+	Admin.find({}, function (err, admins) {
+		if (err) return res.status(500).send("There was a problem finding the admins.");
+		res.status(200).send(admins);
 	});
 });
 
-// GETS A SINGLE USER FROM THE DATABASE
+// GETS A SINGLE ADMIN FROM THE DATABASE
 router.get('/:id', function (req, res) {
-	Admin.findById(req.params.id, function (err, user) {
-		if (err) return res.status(500).send("There was a problem finding the user.");
-		if (!user) return res.status(404).send("No user found.");
-		res.status(200).send(user);
+	Admin.findById(req.params.id, function (err, admin) {
+		if (err) return res.status(500).send("There was a problem finding the admin.");
+		if (!admin) return res.status(404).send("No admin found.");
+		res.status(200).send(admin);
 	});
 });
 
 // DELETES A ADMIN FROM THE DATABASE
 router.delete('/:id', function (req, res) {
-	Admin.findByIdAndRemove(req.params.id, function (err, user) {
-		if (err) return res.status(500).send("There was a problem deleting the user.");
-		res.status(200).send("User: " + user.name + " was deleted.");
+	Admin.findByIdAndRemove(req.params.id, function (err, admin) {
+		if (err) return res.status(500).send("There was a problem deleting the admin.");
+		res.status(200).send("Admin: " + admin.name + " was deleted.");
 	});
 });
 
-// UPDATES A SINGLE USER IN THE DATABASE
+// UPDATES A SINGLE ADMIN IN THE DATABASE
 router.put('/:id', function (req, res) {
-	Admin.findByIdAndUpdate(req.params.id, req.body, { new: true }, function (err, user) {
-		if (err) return res.status(500).send("There was a problem updating the user.");
-		res.status(200).send(user);
+	Admin.findByIdAndUpdate(req.params.id, req.body, { new: true }, function (err, admin) {
+		if (err) return res.status(500).send("There was a problem updating the admin.");
+		res.status(200).send(admin);
 	});
 });
-
-
-/********************************************************************************8
-BELOW ARE ATTEMPTS TO BE ABLE TO GET THE ADMIN TO DELETE/MODIFY DRIVERS -- NOT CURRENTLY WORKING
-***************************************************/
-
-//router.get('/', function (req, res) {
-//	Driver.find({}, function (err, users) {
-//		if (err) return res.status(500).send("There was a problem finding the users.");
-//		res.status(200).send(users);
-//	});
-//});
-
-
-//// DELETES A ADMIN FROM THE DATABASE
-//router.delete('/:id', function (req, res) {
-//	Driver.findByIdAndRemove(req.params.id, function (err, user) {
-//		if (err) return res.status(500).send("There was a problem deleting the user.");
-//		res.status(200).send("User: " + user.name + " was deleted.");
-//	});
-//});
-
-
-//// DELETES A DRIVER FROM THE DATABASE
-//router.delete('/:id', function (req, res) {
-//	Driver.findByIdAndRemove(req.params.id, function (err, user) {
-//		if (err) return res.status(500).send("There was a problem deleting the user.");
-//		res.status(200).send("User: " + user.name + " was deleted.");
-//	});
-//});
-
-
