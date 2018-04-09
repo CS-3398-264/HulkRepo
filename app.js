40801d86d168ce742053f61ebdfc8fe597c555cb
Nano95 <30358490+Nano95@users.noreply.github.com>
2018-04-08 22:33:03 -0500

    added the admin, and Driver Controllers

--- app.js
+++ app.js
@@ -7,4 +7,10 @@ var db = require('./db');
 var UserController = require('./user/userController');
 app.use('/users', UserController);
 
+var AdminController = require('./admin/adminController');
+app.use('/admins', AdminController);
+
+var DriverController = require('./driver/driverController');
+app.use('/drivers', DriverController);
+
 module.exports = app;
