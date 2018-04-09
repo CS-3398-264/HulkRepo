03eb42000c4f044c9ecb24ab22f10f2908a6ac96
Nano95 <30358490+Nano95@users.noreply.github.com>
2018-04-08 22:29:15 -0500

    Added things and cleaned up

--- user/userController.js
+++ user/userController.js
@@ -18,11 +18,42 @@ router.use(bodyParser.json());
 var User = require('./User');
 module.exports = router;
 
+// CREATES A NEW USER -- FOR WHEN ADMIN, OR WHEN USER CREATES AN ACCOUNT WITH NUBER
+router.post('/', function (req, res) {
+	User.create({
+		name: req.body.name,
+		needPickup: req.body.needPickup,
+		longitude: req.body.longitude,
+		latitude: req.body.latitude,
+		rating: req.body.rating
+	},
+        function (err, user) {
+        	if (err) return res.status(500).send("There was a problem adding the information to the database.");
+        	res.status(200).send(user);
+        });
+});
+
 // RETURNS ALL THE USERS IN THE DATABASE
 router.get('/', function (req, res) {
 	User.find({}, function (err, users) {
 		if (err) return res.status(500).send("There was a problem finding the users.");
 		res.status(200).send(users);
+	});
+});
+
+//// UPDATES A SINGLE DRIVER IN THE DATABASE -- SO THAT USER CAN UPDATE HIMSELF AVAILABLE
+router.put('/:id', function (req, res) {
+	User.findByIdAndUpdate(req.params.id, req.body, { new: true }, function (err, user) {
+		if (err) return res.status(500).send("There was a problem updating the user.");
+		res.status(200).send(user);
+	});
+});
+
+//// DELETES A USER FROM THE DATABASE -- ADMINS MAY HAVE ABILITY TO DELETE USERS
+router.delete('/:id', function (req, res) {
+	User.findByIdAndRemove(req.params.id, function (err, user) {
+		if (err) return res.status(500).send("There was a problem deleting the user.");
+		res.status(200).send("User deleted.");
 	});
 });
 
