13b7f7c891ce790e2465420bdad6035ce587d2c3
Nano95 <30358490+Nano95@users.noreply.github.com>
2018-04-11 10:09:09 -0500

    POST & DELETE methods now need credentials
    ONLY ADMINS ALLOWED

--- driver/driverController.js
+++ driver/driverController.js
@@ -8,22 +8,32 @@ router.use(bodyParser.json());
 var Driver = require('./Driver');
 module.exports = router;
 
-// CREATES A NEW DRIVER -- SOME OF THESE FUNCTIONS, IF UI WAS PRESENT
-// SHOULD ONLY BE AUTHORIZED BY AN ADMIN
+// CREATES A NEW DRIVER -- ONLY AVAILABLE TO ADMIN IF HE KNOWS THE CREDENTIALS
 /**first param: route which will be linked to a function. 
 *  Second param: function. 1st param request to server. 2nd response from server
 */
 router.post('/', function (req, res) {
-	Driver.create({
-		name: req.body.name,
-		available: req.body.available,
-		latitude: req.body.latitude,
-		longitude: req.body.longitude
-	},
-        function (err, driver) {
-        	if (err) return res.status(500).send("There was a problem adding the information to the database.");
-        	res.status(200).send(driver);
-        });
+	var credentials = auth(req);
+	if (!credentials || credentials.name !== 'admin' || credentials.pass !== 'secret') 
+	{
+		res.statusCode = 401;
+		res.setHeader('WWW-Authenticate', 'Basic realm="example"');
+		res.end('Access denied');
+	} 
+	else 
+	{
+		Driver.create({
+			name: req.body.name,
+			available: req.body.available,
+			latitude: req.body.latitude,
+			longitude: req.body.longitude
+		},
+		function (err, driver) {
+			if (err) return res.status(500).send("There was a problem adding the information to the database.");
+			res.status(200).send("Access Granted. " + driver.name + " created.\n\n" + driver);
+
+		});
+	}
 });
 
 // GETS A SINGLE USER FROM THE DATABASE
@@ -60,12 +70,20 @@ router.put('/:id', function (req, res) {
 	});
 });
 
-//// DELETES A DRIVER FROM THE DATABASE
+//// DELETES A DRIVER FROM THE DATABASE  -- ONLY AVAILABLE TO ADMIN IF HE KNOWS THE CREDENTIALS
 router.delete('/:id', function (req, res) {
-	Driver.findByIdAndRemove(req.params.id, function (err, driver) {
-		if (err) return res.status(500).send("There was a problem deleting the driver.");
-		res.status(200).send("Driver deleted.");
-	});
+	var credentials = auth(req);
+	if (!credentials || credentials.name !== 'admin' || credentials.pass !== 'secret')
+	{
+		res.statusCode = 401;
+		res.setHeader('WWW-Authenticate', 'Basic realm="example"');
+		res.end('Access denied');
+	}
+	else
+	{
+		Driver.findByIdAndRemove(req.params.id, function (err, driver) {
+			if (err) return res.status(500).send("There was a problem deleting the driver.");
+			res.status(200).send("Access Granted. " + driver.name + " deleted.");
+		});
+	}
 });
-
-
