4070a1736f48ef2fa19203d4a4fdd9101f09568d
Nano95 <30358490+Nano95@users.noreply.github.com>
2018-04-18 13:50:40 -0500

    Added vehicleSize key

--- driver/driver.js
+++ driver/driver.js
@@ -3,7 +3,9 @@ var DriverSchema = new mongoose.Schema({
 	name: String,
 	available: Boolean,
 	latitude: Number,
-	longitude: Number
+	longitude: Number,
+	vehicleSize: String
 });
 mongoose.model('Driver', DriverSchema);
 module.exports = mongoose.model('Driver');
+
