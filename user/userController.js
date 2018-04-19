a2576625d7428d8a406c34c305cf9f110bc22676
Nano95 <30358490+Nano95@users.noreply.github.com>
2018-04-18 21:39:00 -0500

    get and put methods show time and distance

--- user/userController.js
+++ user/userController.js
@@ -12,6 +12,39 @@ var bodyParser = require('body-parser');
 router.use(bodyParser.urlencoded({ extended: true }));
 router.use(bodyParser.json());
 
+//var distance = require('google-distance-matrix');
+var distance = require('google-distance');
+
+/*var origins = ['29.887487, -97.940158'];
+var destinations = ['29.892996, -97.902081', ];
+
+distance.key('AIzaSyDbcwRS0g5wUo1lxrkUBxIJBLLZzRDRlJE');
+distance.units('imperial');
+
+distance.matrix(origins, destinations, function (err, distances) {
+	if (err) {
+		return console.log(err);
+	}
+	if (!distances) {
+		return console.log('no distances');
+	}
+	if (distances.status == 'OK') {
+		for (var i = 0; i < origins.length; i++) {
+			for (var j = 0; j < destinations.length; j++) {
+				var origin = distances.origin_addresses[i];
+				var destination = distances.destination_addresses[j];
+				if (distances.rows[0].elements[j].status == 'OK') {
+					var distance = distances.rows[i].elements[j].distance.text;
+					console.log('Distance from ' + origin + ' to ' + destination + ' is ' + distance);
+				} else {
+					console.log(destination + ' is not reachable by land from ' + origin + '\n\n');
+				}
+			}
+		}
+	}
+});*/
+
+
 // Whenever you create a model like you did in user.js, it automagically 
 // receives all the necessary methods for interacting with 
 // a database, including create, read, update and delete actions
@@ -33,19 +66,87 @@ router.post('/', function (req, res) {
         });
 });
 
-// RETURNS ALL THE USERS IN THE DATABASE
-router.get('/', function (req, res) {
-	User.find({}, function (err, users) {
-		if (err) return res.status(500).send("There was a problem finding the users.");
-		res.status(200).send(users);
+// GETS A SINGLE USER FROM THE DATABASE
+router.get('/:id', function (req, res) {
+	User.findById(req.params.id, function (err, user) {
+		if (err) return res.status(500).send("There was a problem finding the user.");
+		if (!user) return res.status(404).send("No user found.");
+		distance.get({
+			origin: [user.driverLat + ", " + user.driverLon],
+			destination: [user.latitude + ", " + user.longitude],
+			mode: 'driving',
+			units: 'imperial'
+		},
+			  function (err, data) {
+			  	if (err) return console.log(err);
+			  	if (!data) return consoloe.log('no distance');
+			  	console.log("Distance from Driver: " + data.distance + "\nArrival Time: " + data.duration + "\n");
+			  	res.status(200).send("Distance from Driver: " + data.distance + "\nArrival Time: " + data.duration + "\n\n" + user );
+			  });
+		
 	});
 });
 
-//// UPDATES A SINGLE DRIVER IN THE DATABASE -- SO THAT USER CAN UPDATE HIMSELF AVAILABLE
+// RETURNS ALL THE USERS IN THE DATABASE
+router.get('/', function (req, res) {
+	if (req.query.name) {
+		User.find({
+			name: req.query.name
+		}, function (err, user) {
+			if (err) return res.status(500).send("There was a problem finding the user.");
+			res.status(200).send(user);
+		});
+	}
+	else if (req.query.needPickup) {
+		User.find({
+			needPickup: req.query.needPickup
+		}, function (err, user) {
+			if (err) return res.status(500).send("There was a problem finding the user.");
+			res.status(200).send(user);
+		});
+	}
+	else if (req.query.name && req.query.needPickup) {
+		User.find({
+			name: req.query.name,
+			needPickup: req.query.needPickup
+		}, function (err, user) {
+			if (err) return res.status(500).send("There was a problem finding the user.");
+			res.status(200).send(user);
+		});
+	}
+	else if (req.query.rating) {
+		User.find({
+			name: req.query.rating
+		}, function (err, user) {
+			if (err) return res.status(500).send("There was a problem finding the user.");
+			res.status(200).send(user);
+		});
+	}
+	else {
+		User.find({}, function (err, users) {
+			if (err) return res.status(500).send("There was a problem finding the user.");
+			res.status(200).send(users);
+		});
+	}
+});
+
+//// UPDATES A SINGLE USER IN THE DATABASE -- SO THAT USER CAN UPDATE HIMSELF AVAILABLE
 router.put('/:id', function (req, res) {
 	User.findByIdAndUpdate(req.params.id, req.body, { new: true }, function (err, user) {
 		if (err) return res.status(500).send("There was a problem updating the user.");
-		res.status(200).send(user);
+		
+		distance.get({
+			origin: [user.driverLat + ", " + user.driverLon],
+			destination: [user.latitude + ", " + user.longitude],
+			mode: 'driving',
+			units: 'imperial'
+		},
+		function (err, data) {
+			if (err) return console.log(err);
+			if (!data) return console.log('no distance');
+			res.status(200).send("Distance from Driver: " + data.distance + "\nArrival Time: " + data.duration + "\n" + user);
+			console.log("Distance from Driver: " + data.distance + "\nArrival Time: " + data.duration + "\n");
+		});
 	});
 });
 
@@ -56,4 +157,3 @@ router.delete('/:id', function (req, res) {
 		res.status(200).send("User deleted.");
 	});
 });
-
