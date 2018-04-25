9d13cf16138c05935cae8fc33afbe065b3ca1f56
Nano95 <30358490+Nano95@users.noreply.github.com>
2018-04-25 18:44:23 -0500

    Update user.js

--- user/user.js
+++ user/user.js
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-// User.js -- You’re creating a schema which 
-// will give every user in the database a specific look.
-var mongoose = require('mongoose');
-var UserSchema = new mongoose.Schema({
-	name: String,
-	needPickup: Boolean,
-	longitude: Number,
-	latitude: Number,
-	rating: {type: Number, default : 0 },
-	ratingList: [{ type: Number }]
-});
-mongoose.model('User', UserSchema);
-module.exports = mongoose.model('User');
-=======
 // User.js -- You’re creating a schema which 
 // will give every user in the database a specific look.
 var mongoose = require('mongoose');
@@ -23,9 +8,7 @@ var UserSchema = new mongoose.Schema({
 	latitude: Number,
 	driverLat: Number,
 	driverLon: Number,
-	driverID: String,
-	rating: Number
+	driverID: String
 });
 mongoose.model('User', UserSchema);
 module.exports = mongoose.model('User');
->>>>>>> a2576625d7428d8a406c34c305cf9f110bc22676
